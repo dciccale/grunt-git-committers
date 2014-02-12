ad0d141e5a8e09b81894481e4522338611413aa1
Denis Ciccale <dciccale@gmail.com>
2014-02-12 17:58:55 +0100

    Unify code style and add .editorconfig

--- tasks/git-committers.js
+++ tasks/git-committers.js
@@ -2,7 +2,7 @@
  * grunt-git-committers
  * https://github.com/dciccale/grunt-git-committers
  *
- * Copyright (c) 2013 Denis Ciccale (@tdecs)
+ * Copyright (c) 2013-2014 Denis Ciccale (@tdecs)
  * Licensed under the MIT license.
  * https://github.com/dciccale/grunt-git-committers/blob/master/LICENSE-MIT
  */
@@ -11,95 +11,96 @@
 
 module.exports = function (grunt) {
 
-    var _ = grunt.util._;
-    var exec = require('child_process').exec;
-    var os = require('os');
+  var _ = grunt.util._;
+  var exec = require('child_process').exec;
+  var os = require('os');
 
-    grunt.registerTask('committers', 'Get the committers from a git repository with some sorting and formatting options', function () {
-        var options = this.options({
-            sort: 'chronological', // alphabetical, commits
-            email: false, // show emails in the output
-            nomerges: false, // only works when sorting by commits
-            output: './AUTHORS.txt' // the output file
-        });
+  grunt.registerTask('committers', 'Get the committers from a git repository with some sorting and formatting options', function () {
+    var options = this.options({
+      sort: 'chronological', // Alphabetical, commits
+      email: false, // Show emails in the output
+      nomerges: false, // Only works when sorting by commits
+      output: './AUTHORS.txt' // The output file
+    });
 
-        var done = this.async();
+    var done = this.async();
 
-        var _format = function (stdout) {
-            var maxcol = 0;
-            var pad = ' ';
-            return stdout.replace(/^\s+|\s+$/g, '').split('\n').map(function (l) {
-                var numl = l.match(/\d+/);
-                if (numl) {
-                    numl = numl[0].length;
-                    maxcol = numl > maxcol ? numl : maxcol;
-                    pad = '  ' + new Array(maxcol - numl + 1).join(' ');
-                }
-                return _.trim(l.replace(/\t+/, pad));
-            });
-        };
+    var _format = function (stdout) {
+      var maxcol = 0;
+      var pad = ' ';
 
-        // sort types
-        var sortMethod = {
-            alphabetical: 'sort',
-            chronological: 'reverse'
-        };
+      return stdout.replace(/^\s+|\s+$/g, '').split('\n').map(function (l) {
+        var numl = l.match(/\d+/);
 
-        // sort output
-        var _sort = function (stdout) {
-            if (sortMethod[options.sort]) {
-                stdout = _.unique(stdout[sortMethod[options.sort]]());
-            }
-            return stdout;
-        };
-
-        // default command 'git'
-        var cmd = 'git';
-
-        if (options.sort === 'chronological' || options.sort === 'alphabetical') {
-            cmd += ' log --pretty="%aN';
-
-            // show email
-            if (options.email) {
-                cmd += ' <%aE>';
-            }
-
-            cmd += '"';
-
-            // sort by number of commits
-        } else if (options.sort === 'commits') {
-            cmd += ' shortlog -ns';
-
-            // show email
-            if (options.email) {
-                cmd += 'e';
-            }
-
-            // omit merge commits
-            if (options.nomerges) {
-                cmd += ' --no-merges';
-            }
-
-
-            //detect current system use the appropriate terminal
-            if (/win32/.test(process.platform)) {
-                cmd += ' < CON';
-            } else {
-                cmd += ' < /dev/tty';
-            }
+        if (numl) {
+          numl = numl[0].length;
+          maxcol = numl > maxcol ? numl : maxcol;
+          pad = '  ' + new Array(maxcol - numl + 1).join(' ');
         }
 
+        return _.trim(l.replace(/\t+/, pad));
+      });
+    };
 
-        exec(cmd, function (error, stdout, stderr) {
-            if (!error) {
-                stdout = _format(stdout);
-                stdout = _sort(stdout);
-                grunt.file.write(options.output, stdout.join(os.EOL));
-                grunt.log.writeln('File "' + options.output + '" created.');
-                done();
-            } else {
-                grunt.fail.warn(error);
-            }
-        });
+    // Sort types
+    var sortMethod = {
+      alphabetical: 'sort',
+      chronological: 'reverse'
+    };
+
+    // Sort output
+    var _sort = function (stdout) {
+      if (sortMethod[options.sort]) {
+        stdout = _.unique(stdout[sortMethod[options.sort]]());
+      }
+      return stdout;
+    };
+
+    // Default command 'git'
+    var cmd = 'git';
+
+    if (options.sort === 'chronological' || options.sort === 'alphabetical') {
+      cmd += ' log --pretty="%aN';
+
+      // Show email
+      if (options.email) {
+        cmd += ' <%aE>';
+      }
+
+      cmd += '"';
+
+    // Sort by number of commits
+    } else if (options.sort === 'commits') {
+      cmd += ' shortlog -ns';
+
+      // Show email
+      if (options.email) {
+        cmd += 'e';
+      }
+
+      // Omit merge commits
+      if (options.nomerges) {
+        cmd += ' --no-merges';
+      }
+
+      // Detect current system use the appropriate terminal
+      if (/win32/.test(process.platform)) {
+        cmd += ' < CON';
+      } else {
+        cmd += ' < /dev/tty';
+      }
+    }
+
+    exec(cmd, function (error, stdout, stderr) {
+      if (!error) {
+        stdout = _format(stdout);
+        stdout = _sort(stdout);
+        grunt.file.write(options.output, stdout.join(os.EOL));
+        grunt.log.writeln('File "' + options.output + '" created.');
+        done();
+      } else {
+        grunt.fail.warn(error);
+      }
     });
+  });
 };
